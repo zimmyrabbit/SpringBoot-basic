61fe832d2082edecab99566a6f91feb6d155b70b
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-09-06 22:53:43 +0900

    Entity Class 개발

---
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter @Setter
+public class Book extends Item{
+	
+	private String author;
+	
+	private String isbn;
+
+	
+}
