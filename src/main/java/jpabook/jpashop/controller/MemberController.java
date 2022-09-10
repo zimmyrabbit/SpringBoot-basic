37fdc093e057b8a42a93267c68f7ef789b9c294c
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-09-10 22:51:41 +0900

    회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -42,5 +44,13 @@ public class MemberController {
 		
 		memberService.join(member);
 		return "redirect:/";
+	}
+	
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		
+		return "/members/memberList";
 	}
 }
