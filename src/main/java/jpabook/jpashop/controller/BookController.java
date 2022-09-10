4da8b9973c8798e930171881b402ec8e566fff70
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-09-11 00:02:21 +0900

    상품 등록

---
+++ src/main/java/jpabook/jpashop/controller/BookController.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+	private final ItemService itemService;
+	
+	@GetMapping("/items/new")
+	public String createForm(Model model) {
+		model.addAttribute("form", new BookForm());
+		return "items/createItemForm";
+	}
+	
+	@PostMapping("/items/new")
+	public String create(BookForm bookForm) {
+		Book book = new Book();
+		book.setName(bookForm.getName());
+		book.setPrice(bookForm.getPrice());
+		book.setStockQuantity(bookForm.getStockQuantity());
+		book.setAuthor(bookForm.getAuthor());
+		book.setIsbn(bookForm.getIsbn());
+		
+		itemService.saveItem(book);
+		
+		return "redirect:/items";
+	}
+}
