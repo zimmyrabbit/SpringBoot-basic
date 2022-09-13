873b5d931a30c452b0274a9980011d28213be763
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-09-13 22:58:43 +0900

    회원조회 API 생성

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -49,6 +53,22 @@ public class MemberApiController {
 		return new UpdateMemberResponse(member.getId(), member.getName());
 	}
 	
+	@GetMapping("api/v1/members")
+	public List<Member> member1() {
+		return memberService.findMembers();
+	}
+	
+	@GetMapping("api/v2/members")
+	public Result member2() {
+		List<Member> findMembers = memberService.findMembers();
+		
+		List<MemberDto> collect = findMembers.stream()
+				.map(m -> new MemberDto(m.getName()))
+				.collect(Collectors.toList());
+		
+		return new Result(collect.size(), collect);
+	}
+	
 	
 	@Data
 	static class CreateMemberRequest {
@@ -74,6 +94,19 @@ public class MemberApiController {
 	@AllArgsConstructor
 	static class UpdateMemberResponse {
 		private Long id;
+		private String name;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class Result<T> {
+		private int count;
+		private T data;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class MemberDto {
 		private String name;
 	}
 	
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
