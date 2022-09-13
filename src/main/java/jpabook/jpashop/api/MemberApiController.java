26f8faa4cecdbcbd31669d708de722f1bcaf0f9f
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-09-13 22:39:35 +0900

    회원수정 API 생성

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,12 +3,15 @@ package jpabook.jpashop.api;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -38,6 +41,14 @@ public class MemberApiController {
 		return new CreateMemberResponse(id);
 	}
 	
+	@PutMapping("api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
+		
+		memberService.update(id, request.getName());
+		Member member = memberService.findOne(id);
+		return new UpdateMemberResponse(member.getId(), member.getName());
+	}
+	
 	
 	@Data
 	static class CreateMemberRequest {
@@ -53,4 +64,17 @@ public class MemberApiController {
 			this.id = id;
 		}
 	}
+	
+	@Data
+	static class UpdateMemberRequest {
+		private String name;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
+	}
+	
 }
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -52,4 +52,10 @@ public class MemberService {
 		
 		return member;
 	}
+	
+	@Transactional
+	public void update(Long id, String name) {
+		Member member = memberRepository.findOne(id);
+		member.setName(name);
+	}
 }
